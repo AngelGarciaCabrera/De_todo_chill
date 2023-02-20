f15bbe698b246b1956bf767efe56dc7123c93952
LinkFalcon0921 <20198918@itla.edu.do>
2023-02-19 21:21:33 -0400

    Adding test to interface to handle client.

---
+++ De_Tod_chill.Datos/Entities/ICliente.cs
@@ -0,0 +1,6 @@
+﻿namespace De_Tod_chill.Datos.Entities;
+
+public interface ICliente<TId>
+{
+    TId GetId();
+}
---
+++ De_Tod_chill.Datos/Entities/User.cs
@@ -0,0 +1,19 @@
+﻿namespace De_Tod_chill.Datos.Entities;
+
+public class User : ICliente<int> 
+{
+    public int Id { set; get; }
+    public string Name { set; get; }
+    public string Rol { get; set; }
+    public string Correo { get; set; }
+
+    public User(int id)
+    {
+        Id = id;
+    }
+
+    public int GetId()
+    {
+        return Id;
+    }
+}
