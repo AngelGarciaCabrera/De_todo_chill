1e09fe5b66102ff80386145f2b1da6ca7c322222
angel garcia <angel001garcia@hotmail.com>
2023-02-19 20:13:39 -0500

    cliente entities

---
+++ De_Tod_chill.Datos/Entities/Cliente.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace De_Tod_chill.Datos.Entities
+{
+    internal class Cliente
+
+    {
+        public Cliente()
+        {
+
+        }
+        public string Name { get; set; }
+        public int ClienteID { get; set; }
+
+        public string Rol { get; set; }
+        public string correo { get; set; }
+
+    }
+}
