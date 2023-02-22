306a8df28e7d146b29dd7bea0ea066a90b94b198
LinkFalcon0921 <20198918@itla.edu.do>
2023-02-21 21:10:23 -0400

    Just for example.

---
+++ De_Tod_chill.Datos/Contexts/DBContext.cs
@@ -0,0 +1,12 @@
+﻿using De_Tod_chill.Datos.Entities;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace De_Tod_chill.Datos.Contexts;
+
+public class DBContext : DbContext
+{
+
+    public DbSet<User> dbUser { set; get; }
+    
+}
---
+++ De_Tod_chill.Datos/Program.cs
@@ -0,0 +1,14 @@
+﻿using De_Tod_chill.Datos.Entities;
+
+namespace De_Tod_chill.Datos;
+
+public class Program
+{
+    public static void Main()
+    {
+        List<ICliente<object>> clientes = new List<ICliente<object>>();
+
+        ICliente<string> admin = new Admin();
+        // clientes.Add(admin);
+    }
+}
