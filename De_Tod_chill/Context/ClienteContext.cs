d4ce1c12afee6a0a45641c48d76b275f98d469d5
angel garcia <angel001garcia@hotmail.com>
2023-02-21 20:11:06 -0500

    BD

---
+++ De_Tod_chill/Context/ClienteContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace De_Tod_chill.Context
+{
+    public class ClienteContext : DbContext
+    {
+        public ClienteContext(DbContextOptions<ClienteContext> options): base(options)
+
+        {
+
+        }
+        public
+
+
+    }
+}
