4e8d807cf767c2c614f7af3e3c71a537cac0e113
LinkFalcon0921 <20198918@itla.edu.do>
2023-03-03 17:31:58 -0400

    Moving entities to Database project.

--- De_Tod_chill.Database/Contexts/MySqlContext.cs
+++ De_Tod_chill.Database/Contexts/MySqlContext.cs
@@ -1,8 +1,13 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using De_Tod_chill.Datos.Entities.Customers;
+using Microsoft.EntityFrameworkCore;
 
 namespace De_Tod_chill.Database.Contexts;
 
 public class MySqlContext : DbContext
 {
+    public MySqlContext(DbContextOptions options) : base(options)
+    {
+    }
     
+    public DbSet<Customer> Customers { set; get; }
 }
--- De_Tod_chill.Datos/Program.cs
+++ De_Tod_chill.Datos/Program.cs
@@ -1,6 +1,4 @@
-﻿using De_Tod_chill.Datos.Entities;
-using De_Tod_chill.Datos.Entities.Customers;
-
+﻿
 namespace De_Tod_chill.Datos;
 
 public class Program
