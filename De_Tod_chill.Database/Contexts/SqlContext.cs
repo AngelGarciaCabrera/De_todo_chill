f300eec426cb9bf589bfe27c17638b9ac4605826
angel garcia <angel001garcia@hotmail.com>
2023-03-03 17:13:35 -0500

    porfin conO

---
+++ De_Tod_chill.Database/Contexts/SqlContext.cs
@@ -0,0 +1,13 @@
+﻿using De_Tod_chill.Datos.Entities.Customers;
+using Microsoft.EntityFrameworkCore;
+
+namespace De_Tod_chill.Database.Contexts;
+
+public class SqlContext : DbContext
+{
+    public SqlContext(DbContextOptions options) : base(options)
+    {
+    }
+    
+    public DbSet<Customer> Customers { set; get; }
+}
