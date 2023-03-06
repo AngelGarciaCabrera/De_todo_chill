af7dfaa677f40149d6ba1efd9433c702ce298f06
angel garcia <angel001garcia@hotmail.com>
2023-03-05 20:13:56 -0500

    add length in the description

--- De_Tod_chill.Database/Entities/Products/Product.cs
+++ De_Tod_chill.Database/Entities/Products/Product.cs
@@ -11,6 +11,7 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public double Price { get; set; }
+        [MaxLength(100)][MinLength(2)]
         public string Description { get; set; }
         public string Category { get; set; }
 
